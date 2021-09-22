fecda07b0e1fb3fb1f0640ed984ae8b7c6fe48bc
Alexander Prins <57255355+alex651@users.noreply.github.com>
2021-09-22 19:26:36 +0000

    pipes toegevoegd

--- sketch.js
+++ sketch.js
@@ -1,14 +1,25 @@
 var bird;
-
+var pipes = [];
 function setup() {
   createCanvas(400, 600);
   bird = new bird();
+  pipes.push(new pipe());
 }
 
 function draw() {
   background(0);
   bird.update();
   bird.show();
+
+  if (frameCount % 75 == 0) {
+    pipes.push(new pipe());
+  }
+
+  for (var i = 0; i < pipes.length; i++) {
+    pipes[i].show();
+    pipes[i].update();
+
+  }
 }
 
 function keyPressed(){
