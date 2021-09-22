994971990ee081e005a5be405beb7c2628321ff1
Alexander Prins <57255355+alex651@users.noreply.github.com>
2021-09-22 16:43:08 +0000

    bird schiet niet snel omhoog na snel achterelkaar klikken

--- bird.js
+++ bird.js
@@ -2,7 +2,8 @@ function bird(){
   this.y = height/2;
   this.x = 64;
   
-  this.gravity = 1;
+  this.gravity = 0.6;
+  this.lift = -15;
   this.velocity = 0;
   
   this.show = function() {
@@ -11,11 +12,12 @@ function bird(){
   }
 
   this.up = function() {
-    this.velocity += -this.gravity*10;
+    this.velocity += this.lift;
   }
 
   this.update = function()  {
     this.velocity += this.gravity;
+    this.velocity *= 0.9;
     this.y += this.velocity;
 
     if (this.y > height) {
