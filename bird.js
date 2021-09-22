14329b4df55f1a622a49607db14480922ece4962
Alexander Prins <57255355+alex651@users.noreply.github.com>
2021-09-22 07:45:54 +0000

    class bird gedaan

---
+++ bird.js
@@ -0,0 +1,12 @@
+class Bird {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.r = 50;
+    this.gravity = 2;
+    this.velocity = 0;
+    this.lift = -30;
+    this.friction = 0.1;
+    this.score = 0;
+    this.up = false;
+  }
