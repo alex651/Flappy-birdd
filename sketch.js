dd2514b2e6ad0f0ee22e6465a93bc3ac488c9865
Alexander Prins <57255355+alex651@users.noreply.github.com>
2021-09-27 07:59:06 +0000

    boomstam pipes ipv apen

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ let birdImg, pipeRevImg, pipeImg;
 function preload() {
   birdImg = loadImage("banaan.png");
   backgroundImg = loadImage("achtergrond.jpg")
-  pipeRevImg = loadImage("download.png")
+  pipeRevImg = loadImage("pipe.png")
   pipeImg = loadImage("pipe.png")
 }
 
